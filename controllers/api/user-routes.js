63fa1b96941876de254725b7bb5cc36490490e9d
Khai Truong <kminht@gmail.com>
2022-05-29 10:39:57 -0400

    fix user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,18 +1,69 @@
 const router = require("express").Router();
+const { title } = require("process");
 const { User, Post, Comment } = require("../../models");
 
+// get api.users
+// get all users
+router.get("/", (req, rest) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET api/user/?
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id,", "title", "post_content", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "create_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  })
+
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        // if user with nonexistent id value is searched, send back a 404 status code to indicate the wrong piece of data was requested
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // POST /api/users
 router.post("/", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
+    email: req.body.email,
     password: req.body.password,
   }).then((dbUserData) => {
     req.session.save(() => {
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
+      req.session.loggedIn = true; // if logged in true
 
       res.json(dbUserData);
     });
@@ -47,18 +98,15 @@ router.post("/login", (req, res) => {
   });
 });
 
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  }
-  else {
+  } else {
     res.status(404).end();
   }
-
 });
-
 
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
